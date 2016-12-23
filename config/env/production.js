67fc2f781cecd3ffef1dd5c3d5e807bf798926be
Jared Mosier <Jared Mosier>
2016-12-23 15:42:00 -0700

    config file fix

--- config/env/production.js
+++ config/env/production.js
@@ -23,7 +23,8 @@ module.exports = {
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/angular-ui-utils/ui-utils.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        'public/lib/angular-scroll/angular-scroll.js'
+        'public/lib/angular-scroll/angular-scroll.js',
+        'public/lib/angular-drag-and-drop-lists/angular-drag-and-drop-lists.js'
       ]
     },
     css: [
